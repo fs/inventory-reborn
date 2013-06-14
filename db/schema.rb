0229617a2fb7198ca16d8dc8a7dfa9034bcb5109
Artem Biserov <artem.biserov@flatstack.com>
2013-06-14 15:49:05 +0400

    create model room

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130319140714) do
+ActiveRecord::Schema.define(:version => 20130614112947) do
+
+  create_table "rooms", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
