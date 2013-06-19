86d7c1f1d09fdefb1efebd57cdcfe6c59e7e5955
Artem Biserov <artem.biserov@flatstack.com>
2013-06-19 11:33:58 +0400

    add specs for unit serializer

--- app/serializers/unit_serializer.rb
+++ app/serializers/unit_serializer.rb
@@ -1,5 +1,5 @@
 class UnitSerializer < BaseSerializer
-  attributes :unit_type, :inv_id
+  attributes :unit_type, :inv_id, :name, :description, :on_depot, :out_of_order
 
   has_one :user, :room
 end
