e926aa77f9a0601b35c735157fd6e0139296e110
Artem Biserov <artem.biserov@flatstack.com>
2013-07-01 13:40:18 +0400

    add endpoint create unit

--- app/api_tasters/units.rb
+++ app/api_tasters/units.rb
@@ -31,4 +31,12 @@ ApiTaster.routes do
   get '/units/:id',
     id: Examples.unit.id,
     authentication_token: Examples.user.authentication_token
+
+  desc <<-markdown.strip_heredoc
+    ## Create unit
+  markdown
+
+  post '/units',
+    { unit: FactoryGirl.attributes_for(:unit).merge({ room_id: Examples.room.id }) }.
+      merge({ authentication_token: Examples.user.authentication_token })
 end if defined? ApiTaster
