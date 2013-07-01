865f5d9642c8ada3a703ae004e97616e6318de12
Artem Biserov <artem.biserov@flatstack.com>
2013-07-01 13:40:18 +0400

    add endpoint create unit

--- app/controllers/units_controller.rb
+++ app/controllers/units_controller.rb
@@ -3,7 +3,7 @@ class UnitsController < ApplicationController
 
   expose(:unit_fetcher) { UnitFetcher.perform(params) }
   expose(:units) { unit_fetcher.units }
-  expose(:unit)
+  expose(:unit, attributes: :unit_params)
 
   def index
     respond_with units,
