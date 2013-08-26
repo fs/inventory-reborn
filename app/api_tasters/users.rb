40483c9d54b86fd5a39b1aa68746e582f38fa3e1
ildarsafin <ildar.safin@flatsourcing.com>
2013-08-26 13:56:07 +0000

    fixed users.rb in api_taster

--- app/api_tasters/users.rb
+++ app/api_tasters/users.rb
@@ -3,7 +3,7 @@ ApiTaster.routes do
     ## List all users
   markdown
 
- get '/users',
-  authentication_token: Examples.user.authentication_token
-  
-end
+  get '/users',
+    authentication_token: Examples.user.authentication_token
+
+end if defined? ApiTaster
